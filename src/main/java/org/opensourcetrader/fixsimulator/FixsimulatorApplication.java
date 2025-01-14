af2edf5615372e4af716376bb88088df031f67ff
Komal Thakur <thakurkomal7260@gmail.com>
2025-01-14 23:09:08 +0530

    init

---
+++ src/main/java/org/opensourcetrader/fixsimulator/FixsimulatorApplication.java
@@ -0,0 +1,13 @@
+package org.opensourcetrader.fixsimulator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FixsimulatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FixsimulatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/opensourcetrader/fixsimulator/FixsimulatorApplicationTests.java
@@ -0,0 +1,14 @@
+package org.opensourcetrader.fixsimulator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FixsimulatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+		//initial commit
+	}
+
+}
